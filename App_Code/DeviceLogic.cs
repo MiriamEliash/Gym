20a48cbdee7ff7359984a9011c92801212c295f5
MiriamEliash <74762021+MiriamEliash@users.noreply.github.com>
2020-11-20 10:16:17 +0200

    Add files via upload

---
+++ App_Code/DeviceLogic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Gym.App_Code
+{
+    public class DeviceLogic
+    {
+        DAL dal = new DAL();
+        public DataSet getAllDevice()
+        {
+            string sql = "SELECT * FROM Devices";
+            return dal.excuteQuery(sql);
+        }
+    }
+}
---
+++ App_Code/SubscribersLogic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Gym.App_Code
+{
+
+     DAL dal = new DAL();
+    public class SubscribersLogic
+    {
+        public bool checkExist (string ID, string pass)
+        {
+            string sql = "SELECT * FROM Subscribers WHERE ID='" + ID + "' and Password='" + pass + "'"; ;
+            DataSet ds = dal.excuteQuery(sql);
+            if (ds.Tables[0].Rows.Count < 0)
+                return false;
+            return true;
+        }
+        public void addNewSubscriber(string ID,string FirstName, string LastName, string PhoneNumber, string Weight, string Pass2)
+        {
+            string sql = String.Format("INSERT INTO Users (ID,First name,Last name,Phone number,Weight,Password) VALUES ('{0}', '{1}','{2}','{3}','{4}','{5}','{6}')", ID, FirstName, LastName, PhoneNumber, Weight, Pass2);
+           // dal.excuteQuery(sql);
+        }
+    }
+
+    }
+}
